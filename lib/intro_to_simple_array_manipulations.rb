cd00d97d512e2f227142ca36090a2edc2f25a0af
Austin Trow <austintrow@gmail.com>
2019-07-01 01:31:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,6 +6,11 @@ def using_unshift(arr, string)
   arr.unshift(string)
 end
 
-def using_pop(array)
-  array.pop
-end  
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args(arr)
+  arr.pop(2)
+end
+
